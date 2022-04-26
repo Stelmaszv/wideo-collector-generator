aadb5eaeeccc18645de351aeb708bda13ca18d89
= <stelmaszv@gmail.com>
2022-04-26 15:28:50 +0200

    Add FactoryScan, Class AbstractScanElement and instance off it

--- core/dir.py
+++ core/dir.py
@@ -8,56 +8,71 @@ class ScanDirs:
 
     def start(self):
         scan_dir= {
-            "stars"  :ScanStars,
-            "series" :ScanSeries,
-            "producents": ScanProducents,
+            "stars"  :StarsDir,
+            "series" :SeriesDir,
+            "producents": ProducentsDir,
         }
         for dir in self.dir:
             if dir != "movies":
                 print('Scaning Dir ... Dir '+dir+'')
                 dir=scan_dir[dir](self.dir[dir])
-                dir.init_dir()
                 dir.scan()
 
 
 class AbstractScan(ABC):
     base_dir=['A-D','E-H','I-L','M-P','R-U','W-Z']
+    FactoryScan = None
     def __init__(self,dir):
         self.dir=dir
+        self.base_init_dir()
 
-    @abstractmethod
     def scan(self):
-        pass
+        dir_list = os.listdir(self.dir)
+        for dir in dir_list:
+            is_dir = os.path.isdir(self.dir + '\\' + dir)
+            if is_dir:
+                dir_list_elemnts = os.listdir(self.dir + '\\' + dir)
+                for el in dir_list_elemnts:
+                    self.FactoryScan(self.dir + '\\' + dir + '\\' + el).scan()
 
-    @abstractmethod
     def init_dir(self):
-        pass
+        self.base_init_dir()
 
     def base_init_dir(self):
         for dir in self.base_dir:
             if os.path.isdir(self.dir+'\\'+dir) is False:
                 os.mkdir(self.dir+'\\'+dir)
 
-class ScanStars(AbstractScan):
+class AbstractScanElement(ABC):
+
+    def __init__(self,dir):
+        self.dir = dir
+
+    @abstractmethod
+    def scan(self):
+        pass
+
+class ScanSerie(AbstractScanElement):
 
     def scan(self):
         pass
 
-    def init_dir(self):
-        self.base_init_dir()
-
-class ScanSeries(AbstractScan):
+class ScanStar(AbstractScanElement):
 
     def scan(self):
         pass
 
-    def init_dir(self):
-        self.base_init_dir()
-
-class ScanProducents(AbstractScan):
+class ScanProducent(AbstractScanElement):
 
     def scan(self):
         pass
 
-    def init_dir(self):
-        self.base_init_dir()
+class StarsDir(AbstractScan):
+    FactoryScan = ScanStar
+
+class SeriesDir(AbstractScan):
+    FactoryScan = ScanSerie
+
+class ProducentsDir(AbstractScan):
+    FactoryScan = ScanProducent
+
