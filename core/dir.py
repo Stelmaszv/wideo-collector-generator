7fb8705467db15b8f09181ea247f0387e4933e07
= <stelmaszv@gmail.com>
2022-04-26 14:59:31 +0200

    Add base_dir in Class "AbstractScan"

--- core/dir.py
+++ core/dir.py
@@ -1,3 +1,4 @@
+import os
 from abc import ABC, abstractmethod
 
 class ScanDirs:
@@ -14,10 +15,13 @@ class ScanDirs:
         for dir in self.dir:
             if dir != "movies":
                 print('Scaning Dir ... Dir '+dir+'')
-                scan_dir[dir](self.dir[dir]).scan()
+                dir=scan_dir[dir](self.dir[dir])
+                dir.init_dir()
+                dir.scan()
+
 
 class AbstractScan(ABC):
-
+    base_dir=['A-D','E-H','I-L','M-P','R-U','W-Z']
     def __init__(self,dir):
         self.dir=dir
 
@@ -25,17 +29,35 @@ class AbstractScan(ABC):
     def scan(self):
         pass
 
+    @abstractmethod
+    def init_dir(self):
+        pass
+
+    def base_init_dir(self):
+        for dir in self.base_dir:
+            if os.path.isdir(self.dir+'\\'+dir) is False:
+                os.mkdir(self.dir+'\\'+dir)
+
 class ScanStars(AbstractScan):
 
     def scan(self):
         pass
+
+    def init_dir(self):
+        self.base_init_dir()
 
 class ScanSeries(AbstractScan):
 
     def scan(self):
         pass
 
+    def init_dir(self):
+        self.base_init_dir()
+
 class ScanProducents(AbstractScan):
 
     def scan(self):
         pass
+
+    def init_dir(self):
+        self.base_init_dir()
