4ee6c06f7e479921ef2f168c69998141c438e86f
= <stelmaszv@gmail.com>
2022-04-27 12:35:56 +0200

    Add scan movies in series

--- core/dir.py
+++ core/dir.py
@@ -1,5 +1,6 @@
 import os
 from abc import ABC, abstractmethod
+from pathlib import Path
 
 class ScanDirs:
 
@@ -17,7 +18,6 @@ class ScanDirs:
                 print('Scaning Dir ... Dir '+dir+'')
                 dir=scan_dir[dir](self.dir[dir])
                 dir.scan()
-
 
 class AbstractScan(ABC):
     base_dir=['A-D','E-H','I-L','M-P','R-U','W-Z']
@@ -45,17 +45,42 @@ class AbstractScan(ABC):
 
 class AbstractScanElement(ABC):
 
+    scan_dir= ''
+
     def __init__(self,dir):
         self.dir = dir
+        self.init_dir()
+        self.create_json_config()
 
     @abstractmethod
     def scan(self):
         pass
 
+    def create_json_config(self):
+        if Path(self.dir + '\\config.json').is_file() is False:
+            f = open(self.dir + '\\config.json', "x")
+            f.write('{}')
+            f.close()
+
+    def init_dir(self):
+        for dir in self.base_dir:
+            if os.path.isdir(self.dir + '\\' + dir) is False:
+                os.makedirs(self.dir + '\\' + dir)
+
+
 class ScanSerie(AbstractScanElement):
 
+    scan_dir = 'movies'
+    base_dir=['movies','photos\\DATA','banners','stars']
+
     def scan(self):
-        pass
+        dir_list = os.listdir(self.dir + '\\' + self.scan_dir);
+        for dir in dir_list:
+            is_dir = os.path.isdir(self.dir + '\\' + self.scan_dir + '\\' + dir)
+            if is_dir:
+                dir_list_el = os.listdir(self.dir + '\\' + self.scan_dir + '\\' + dir+'\\DATA')
+                for el_in_dir in dir_list_el:
+                    print(el_in_dir)
 
 class ScanStar(AbstractScanElement):
 
@@ -65,7 +90,9 @@ class ScanStar(AbstractScanElement):
 class ScanProducent(AbstractScanElement):
 
     def scan(self):
-        pass
+        dir_list = os.listdir(self.dir+'\\'+self.scan_dir);
+        for dir in dir_list:
+            print(dir)
 
 class StarsDir(AbstractScan):
     FactoryScan = ScanStar
@@ -75,4 +102,7 @@ class SeriesDir(AbstractScan):
 
 class ProducentsDir(AbstractScan):
     FactoryScan = ScanProducent
+
+
+
 
